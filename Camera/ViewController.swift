f40a19db1d47c164bc1f4d0066004b5fad5b1a91
Kurt Werle <kwerle@solarcity.com>
2015-03-20 12:12:06 -0700

    WIP

--- Camera/ViewController.swift
+++ Camera/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempButton: UIButton!
+    @IBOutlet weak var cameraView: CameraView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+//    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+//    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
