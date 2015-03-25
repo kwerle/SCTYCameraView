fa06a30f5be1e8356fb14251117f7742fbf47097
Kurt Werle <kurt@CircleW.org>
2015-03-25 00:44:10 -0700

    Initial 2

---
+++ SCTYCameraView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCTYCameraView
+//
+//  Created by Kurt Werle on 3/24/15.
+//  Copyright (c) 2015 Kurt Werle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCTYCameraView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCTYCameraView
+//
+//  Created by Kurt Werle on 3/24/15.
+//  Copyright (c) 2015 Kurt Werle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
